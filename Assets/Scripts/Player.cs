e55d91f927c0c0c0f288f1a2e0a097579fa73506
Jeff Biggs <jeff.biggs@simperative.com>
2019-11-30 19:24:19 -0600

    adds mesh based wall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public string m_PlayerName = "name";
     public float m_PlayerSpeedPerSec = 0.2f;
 
     public KeyCode m_LeftTurnKey;
@@ -14,8 +15,13 @@ public class Player : MonoBehaviour
     public KeyCode m_RightTurnKey;
     private bool m_IsRightDown = false;
 
+    readonly Vector3 m_ForwardVec = new Vector3(0f, 1f, 0f);
+    readonly Vector3 m_RightVec = new Vector3(1f, 0f, 0f);
+
     // true if player still alive
     private bool m_IsLiving = true;
+
+    private bool m_IsRebuildWall;
 
     public Color m_WallColor = Color.yellow;
     public float m_WallWidth = 3f;
@@ -30,8 +36,11 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        AddWall();
+        m_IsRebuildWall = true;
     }
+
+    Vector3 PlayerForwardDirection() { return transform.rotation * m_ForwardVec; }
+    Vector3 PlayerRightDirection() { return transform.rotation * m_RightVec; }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -42,10 +51,15 @@ public class Player : MonoBehaviour
     private void AddWall()
     {
         m_CurrentWall = new GameObject();
+        m_CurrentWall.transform.parent = transform.root;
+        m_CurrentWall.name = m_PlayerName + "Wall";
+        
         MeshRenderer renderer = m_CurrentWall.AddComponent<MeshRenderer>();
-        MeshFilter mfilter = m_CurrentWall.AddComponent<MeshFilter>();
-        m_WallMesh = mfilter.mesh;
-        m_WallMesh = new Mesh();
+
+        Shader shader = Shader.Find("Unlit/Color");
+        Material mat = new Material(shader);
+        renderer.material = mat;
+        mat.SetColor("_Color", m_WallColor);
         
         // 4 vertices for two triangles
         Vector3[] triVerts = new Vector3[4];
@@ -53,16 +67,41 @@ public class Player : MonoBehaviour
         int[] triIndices = new int[6];
         
         // position the vertices
-        //triVerts[0] = transform.position + transform 
+        Vector3 startPos = transform.position;
+        startPos.z = -5f;
         
+        triVerts[0] = startPos - PlayerRightDirection() * m_WallWidth;
+        triVerts[1] = startPos + PlayerRightDirection() * m_WallWidth;
+
+        triVerts[2] = startPos - PlayerRightDirection() * m_WallWidth;
+        triVerts[3] = startPos + PlayerRightDirection() * m_WallWidth;
+
+        triIndices[0] = 0;
+        triIndices[1] = 1;
+        triIndices[2] = 2;
+        triIndices[3] = 2;
+        triIndices[4] = 1;
+        triIndices[5] = 3;
+
+        m_WallMesh = new Mesh();
         m_WallMesh.Clear();
         m_WallMesh.vertices = triVerts;
         m_WallMesh.triangles = triIndices;
+
+        MeshFilter mfilter = m_CurrentWall.AddComponent<MeshFilter>();
+        mfilter.mesh = m_WallMesh;
+
     }
 
     private void UpdateWall()
     {
-        
+        if (m_WallMesh != null) {
+            Vector3[] triVerts = m_WallMesh.vertices;
+            triVerts[2] = transform.position - PlayerRightDirection() * m_WallWidth;
+            triVerts[3] = transform.position + PlayerRightDirection() * m_WallWidth;
+
+            m_WallMesh.vertices = triVerts;
+        }
     }
 
     private float HandleInput(float turn)
@@ -70,28 +109,23 @@ public class Player : MonoBehaviour
         if (!m_IsLeftDown && Input.GetKey(m_LeftTurnKey)) {
             m_IsLeftDown = true;
             turn += 90.0f;
-            AddWall();
+            // XXX TODO end wall here!
+            m_IsRebuildWall = true;
         } else if (!Input.GetKey(m_LeftTurnKey)) {
+            // clear flag
             m_IsLeftDown = false;
-            AddWall();
         }
         if (!m_IsRightDown && Input.GetKey(m_RightTurnKey)) {
             m_IsRightDown = true;
             turn -= 90.0f;
-            AddWall();
+            // XXX TODO end wall here!
+            m_IsRebuildWall = true;
         } else if (!Input.GetKey(m_RightTurnKey)) {
+            // clear flag
             m_IsRightDown = false;
-            AddWall();
         }
 
         return turn;
-    }
-
-    Vector3 GetPlayerDirection()
-    {
-        // returns player direction unit vector
-        // assumes forward direction is Y axis (green)
-        return transform.rotation * new Vector3(0f, 1f, 0f); 
     }
 
     // Update is called once per frame
@@ -107,9 +141,16 @@ public class Player : MonoBehaviour
             transform.rotation = Quaternion.Euler(euler.x, euler.y, turn);
             float speed = m_PlayerSpeedPerSec * Time.deltaTime;
             
-            transform.position += -GetPlayerDirection() * speed;
-
-            UpdateWall();
+            transform.position += -PlayerForwardDirection() * speed;
         }
+
+        if (m_IsRebuildWall) {
+            m_IsRebuildWall = false;
+            AddWall();
+        }
+        
+        
+        UpdateWall();
+
     }
 }
